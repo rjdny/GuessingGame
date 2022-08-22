b8105df0a172f167188c497652bea45accf929c1
Ryan Downey <105568842+rjdny@users.noreply.github.com>
2022-08-22 11:13:40 -0500

    Phase6

--- Program.cs
+++ Program.cs
@@ -1,5 +1,28 @@
 ﻿using System;
 
-Console.Write("Guess a secret number:");
-int guessedNumber = int.Parse(Console.ReadLine());
-Console.WriteLine(guessedNumber);
+Console.WriteLine("Guess a secret number:");
+
+int secretNumber = new Random().Next(1,101);
+
+int attempts = 4;
+
+while(attempts > 0)
+{
+    attempts--;
+    int guessedNumber = int.Parse(Console.ReadLine());
+
+    string highOrLow = (guessedNumber > secretNumber)
+    ? "Too high" : "Too low";
+
+    string onWrong = (attempts != 0) 
+        ? $"{highOrLow}... You have {attempts} tries left." 
+        : $"All out of tries.\nThe secret number was {secretNumber}";
+
+    bool correct = (guessedNumber == secretNumber);
+
+    string result = (correct)? "You guessed the secret number!!" : onWrong;
+    Console.WriteLine(result);
+
+    if (correct)
+        break;
+}
